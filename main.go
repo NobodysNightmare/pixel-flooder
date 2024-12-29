c5f6c7c5f66d104f882a88158546ad833510505b
Jan Sandbrink <nobodysnightmare@gmail.com>
2024-12-29 14:06:26 +0100

    Conserve bandwidth by setting offset once

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func main() {
 	for x := minX; x < image.Bounds().Max.X; x++ {
 		for y := minY; y < image.Bounds().Max.Y; y++ {
 			r, g, b, a := image.At(x, y).RGBA()
-			p := Pixel{X: x - minX + dx, Y: y - minY + dy, R: int(r / 256), G: int(g / 256), B: int(b / 256), A: int(a / 256)}
+			p := Pixel{X: x - minX, Y: y - minY, R: int(r / 256), G: int(g / 256), B: int(b / 256), A: int(a / 256)}
 
 			if p.A > transparencyCutoff {
 				pixels = append(pixels, p)
@@ -78,6 +78,9 @@ func main() {
 	connection, err := net.Dial("tcp", remoteAddress)
 	handleError(err)
 	defer connection.Close()
+
+	_, err = connection.Write([]byte(fmt.Sprintf("OFFSET %d %d\n", dx, dy)))
+	handleError(err)
 
 	renderedFrames := 0
 	lastWritten := 0
